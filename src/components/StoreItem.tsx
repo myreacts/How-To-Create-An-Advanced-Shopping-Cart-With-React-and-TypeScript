89ac81ead97961f0eae7525a6901513764107988
baizhi958216 <1475289190@qq.com>
2023-09-29 23:51:54 +0800

    feat: 购物车商品数量修改
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
+import { useShoppingCart } from "../content/ShoppingCartContext";
 
 type StoreItemProps = {
   id: number;
@@ -9,8 +10,15 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const {
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+    getItemQuantity,
+  } = useShoppingCart();
+  const quantity: number = getItemQuantity(id);
   return (
-    <>
+    <Card className="h-100">
       <Card.Img
         variant="top"
         src={imgUrl}
@@ -24,7 +32,34 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
           <span className="fs-2">{name}</span>
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>
+              + Add To Cart
+            </Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{
+                gap: ".5rem",
+              }}
+            >
+              <div className="d-flex align-items-center justify-content-center">
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+                <div className="fs-3">{quantity}</div>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
+              </div>
+              <Button
+                variant="danger"
+                size="sm"
+                onClick={() => removeFromCart(id)}
+              >
+                Remove
+              </Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
-    </>
+    </Card>
   );
 }
